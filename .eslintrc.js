84343d1e12003db52be77173a2a68310b8f3e45c
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2021-11-22 21:56:15 +0800

    add: import normalize.css and add sass loader

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,15 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
     'prettier',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
-  plugins: [
-  ],
+  plugins: [],
   // add your custom rules here
-  rules: {}
-}
+  rules: {},
+};
