e674f3764f0a9ac994a8d45f429a642b0bf43112
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2021-12-20 21:33:38 +0800

    update: add title to the reading page

---
+++ components/atoms/BookTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="book-title">
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
--- components/organisms/BookReadingPages.vue
+++ components/organisms/BookReadingPages.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="book-reading-pages">
-    <BookPagesSwiperNavigator
-      ref="swiper-navigator"
-      class="book-reading-pages-navigator"
-      :page="page"
-      @slideChanged="handleSlideChanged"
-    />
-    <BookPagesSwiper
-      ref="swiper-pages"
-      class="book-reading-pages-swiper"
-      @slide-change="handleSlideSwiped"
-    />
+    <BookTitle :title="$title" />
+    <div class="book-reading-pages-content">
+      <BookPagesSwiperNavigator
+        ref="swiper-navigator"
+        class="book-reading-pages-navigator"
+        :page="page"
+        @slideChanged="handleSlideChanged"
+      />
+      <BookPagesSwiper
+        ref="swiper-pages"
+        class="book-reading-pages-swiper"
+        @slide-change="handleSlideSwiped"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { booksStore } from '@/store';
 
 interface SwiperPagesRef {
   slideTo: (slide: Number) => void;
@@ -26,6 +30,12 @@ export default Vue.extend({
     return {
       page: 0,
     };
+  },
+
+  computed: {
+    $title() {
+      return booksStore.$single?.title;
+    },
   },
 
   methods: {
@@ -49,10 +59,11 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.book-reading-pages {
+.book-reading-pages-content {
   display: grid;
   grid-template-rows: 800px;
   grid-template-columns: 180px 1fr;
   gap: 2rem;
+  margin-top: 1.5rem;
 }
 </style>
